882366a773f29fad03787a7d14dac96872904b48
Daniil Sergeyevich Martyn <daniil.martyn@wsu.edu>
2015-12-02 03:44:07 -0800

    Fixed issue3: All characters and weapons implemented
    They still need work though with animations and projectiles (mines,
    bombs, grenades) need to explode on impact or after a time interval.

--- UC/src/uc/Mine.java
+++ UC/src/uc/Mine.java
@@ -3,6 +3,7 @@ package uc;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 
+import jig.Collision;
 import jig.ConvexPolygon;
 import jig.Entity;
 import jig.ResourceManager;
@@ -30,10 +31,20 @@ public class Mine extends Entity{
 		translate(velocity.scale(delta));
 		velocity = velocity.add(new Vector(0.0f, (PlayState.gravity*delta)));
 		
-		if(getX() < 0 || getX() > PlayState.map.getWidth()  //////////////////change this when map creating is updated
-				|| getY() < 0 || getY() > PlayState.map.getHeight())
+		if(getX() < 0 || getX() > PlayState.map.getImg().getWidth()  //////////////////change this when map creating is updated
+				|| getY() < 0 || getY() > PlayState.map.getImg().getHeight())
 			active = false;
 		
+		
+		Collision collide = null;
+		while((collide = collides(PlayState.map)) != null){
+			System.out.println("collision test: " + collide.getMinPenetration());
+			System.out.println("this shape: " + collide.getThisShape());
+			System.out.println("other shape: " + collide.getOtherShape());
+			
+			translate(collide.getMinPenetration());
+			velocity = new Vector(0.0f, 0.0f);
+		}
 	}
 	
 	public boolean isActive(){
--- UC/src/uc/PlayState.java
+++ UC/src/uc/PlayState.java
@@ -17,7 +17,7 @@ import org.newdawn.slick.state.StateBasedGame;
 public class PlayState extends BasicGameState {
 
 	Char dude;
-	static Image map;
+	public static Map map;
 	
 	public static float gravity = 0.003f;
 	
@@ -37,13 +37,21 @@ public class PlayState extends BasicGameState {
 		// TODO Auto-generated method stub
 
 		UCGame uc = (UCGame) game;
-		map = ResourceManager.getImage(UCGame.TEST_RSC);
-		dude = new Char(map.getWidth()/2, map.getHeight()/2);
-		//particles = new ParticleSystem
+		map = new Map(0,0);
 		
-		
-		offsetMaxX = map.getWidth() - uc.ScreenWidth; 		// offset min/max X/Y technique taken from nathan
-		offsetMaxY = map.getHeight() - uc.ScreenHeight;		// http://gamedev.stackexchange.com/questions/44256/how-to-add-a-scrolling-camera-to-a-2d-java-game
+		switch(UCGame.character){
+		case 0:
+			dude = new Char(map.getImg().getWidth()/2, map.getImg().getHeight()/2, 0);
+			break;
+		case 1: 
+			dude = new Char(map.getImg().getWidth()/2, map.getImg().getHeight()/2, 1);
+			break;
+		case 2:
+			dude = new Char(map.getImg().getWidth()/2, map.getImg().getHeight()/2, 2);
+			break;
+		}
+		offsetMaxX = map.getImg().getWidth() - uc.ScreenWidth; 		// offset min/max X/Y technique taken from nathan
+		offsetMaxY = map.getImg().getHeight() - uc.ScreenHeight;		// http://gamedev.stackexchange.com/questions/44256/how-to-add-a-scrolling-camera-to-a-2d-java-game
 		offsetMinX = 0;										//
 		offsetMinY = 0;										//
 		
@@ -75,7 +83,8 @@ public class PlayState extends BasicGameState {
 
 		g.translate(-camX, -camY);
 		
-		g.drawImage(map, 0, 0);
+		g.drawImage(map.getImg(), 0, 0);
+		map.render(g);
 		dude.render(g);
 		dude.renderWep(g);
 		
@@ -133,33 +142,35 @@ public class PlayState extends BasicGameState {
 			
 		}
 
-		if(input.isMousePressed(0))
+		if(input.isMousePressed(0)){
 			dude.fire();
+			System.out.println("hey mouse is clicked");
+		}
 		
-		dude.update(gravity, container, camX, camY, delta);
+		/*if(input.isMouseButtonDown(0)){
+			dude.fire();
+			System.out.println("hey mouse button is down");
+		}*/
+		
+		dude.update(container, camX, camY, delta);
 		
 		LinkedList<Shape> bounds = dude.getGloballyTransformedShapes();
 		
 		if(dude.getCoarseGrainedMinY() < 0){				// if dude is higher than than the map
 			dude.setY(dude.getCoarseGrainedHeight()/2);
 		}
-		if(bounds.element().getMaxY() > map.getHeight()){	// if dude is lower than the map
-			dude.setY(map.getHeight() - bounds.element().getHeight()/2);
+		if(bounds.element().getMaxY() > map.getImg().getHeight()){	// if dude is lower than the map
+			dude.setY(map.getImg().getHeight() - bounds.element().getHeight()/2);
 			dude.cancelFall();
 			dude.setJump(false);
 		}
 		if (bounds.element().getMinX() < bounds.element().getWidth()){			// if dude is too far left of map
 			dude.setX(bounds.element().getWidth()*1.5f);
 		}
-		if (bounds.element().getMaxX() > map.getWidth() - bounds.element().getWidth()){ // if dude is too far right of map
-			dude.setX(map.getWidth() - bounds.element().getWidth()*1.5f);
+		if (bounds.element().getMaxX() > map.getImg().getWidth() - bounds.element().getWidth()){ // if dude is too far right of map
+			dude.setX(map.getImg().getWidth() - bounds.element().getWidth()*1.5f);
 		}
-		
-		//System.out.println("minX: " + dude.getGloballyTransformedShapes());
-		
-		
-		
-		
+			
 		
 	}
 
